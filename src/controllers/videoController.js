ac8fd536b22fe636992f3c1ae933808de9b00c3b
MangoSteen0903 <sjlee990129@gmail.com>
2021-10-24 02:50:40 +0900

    Heroku/local fix

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -76,12 +76,13 @@ export const postUpload = async (req, res) => {
   } = req.session;
   const { video, thumbnail } = req.files;
   const { title, description, hashtags } = req.body;
+  const isHeroku = process.env.NODE_ENV === "production";
   try {
     const newVideo = await Video.create({
       title,
       description,
-      fileUrl: video[0].location,
-      thumbnailUrl: thumbnail[0].location,
+      fileUrl: isHeroku ? video[0].location : video[0].path,
+      thumbnailUrl: isHeroku ? thumbnail[0].location : video[0].path,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
